560a452f0911ceee9244afbb7ba77932fd87b1c7
Nathan Hicks <nh664186@broadcom.net>
2025-06-23 11:42:24 -0500

    fixing colors v23

--- src/App.js
+++ src/App.js
@@ -59,7 +59,7 @@ export default function App() {
     <div style={{
       minHeight: '100vh',
       minWidth: '100vw',
-      background: 'linear-gradient(to bottom, #fcb6b6, #720000)',
+      background: 'linear-gradient(to bottom,rgb(252, 231, 231),rgb(72, 3, 3))',
     }}>
     <Router>
       <Routes>
