8fb6cac980ff4914598afe5919f1361de3a64d8c
Nathan Hicks <nh664186@broadcom.net>
2025-06-27 14:39:41 -0500

    fixing theme errors

--- src/index.js
+++ src/index.js
@@ -1,8 +1,8 @@
 // src/index.js
 import React from 'react';
 import ReactDOM from 'react-dom/client';
+import './index.css'; // include this BEFORE App
 import App from './App';
-import './index.css'; // Optional, if you have global styles
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
