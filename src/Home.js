d78c40bdc0f4fedfdf7b96367b53c77501937fe5
Nathan Hicks <nh664186@broadcom.net>
2025-07-02 10:15:01 -0500

    fixing home page

--- src/Home.js
+++ src/Home.js
@@ -1,27 +1,26 @@
-import React, { useEffect, useState } from 'react';
+import React from 'react';
 import { Container, Typography } from '@mui/material';
-import { BarChart, Bar, XAxis, YAxis, CartesianGrid, Tooltip, Legend } from 'recharts';
-import Papa from 'papaparse';
+import {
+  BarChart,
+  Bar,
+  XAxis,
+  YAxis,
+  CartesianGrid,
+  Tooltip,
+  Legend
+} from 'recharts';
 
-const SHEET_URL = 'https://docs.google.com/spreadsheets/d/11AI2u8e_q_-99YXX3md2s1qOpClpRJ-959puJFEfw2U/gviz/tq?tqx=out:csv&sheet=Sheet2';
+const staticData = [
+  { Name: 'Enterprise East', Sales: 9500000 },
+  { Name: 'Enterprise West', Sales: 9070000 },
+  { Name: 'HCGE East', Sales: 11290000 },
+  { Name: 'HCGE West', Sales: 7040000 },
+  { Name: 'Mid-Market East', Sales: 11380000 },
+  { Name: 'Mid-Market West', Sales: 12900000 },
+  { Name: 'LATAM', Sales: 6420000 },
+];
 
 export default function Home() {
-  const [data, setData] = useState([]);
-
-  useEffect(() => {
-    fetch(SHEET_URL)
-      .then(res => res.text())
-      .then(csv => {
-        Papa.parse(csv, {
-          header: true,
-          dynamicTyping: true,
-          complete: (results) => {
-            setData(results.data);
-          },
-        });
-      });
-  }, []);
-
   return (
     <Container sx={{ mt: 5 }}>
       <Typography variant="h4" gutterBottom>
@@ -31,7 +30,7 @@ export default function Home() {
       <BarChart
         width={600}
         height={300}
-        data={data}
+        data={staticData}
         margin={{ top: 20, right: 30, left: 20, bottom: 5 }}
       >
         <CartesianGrid strokeDasharray="3 3" />
